07fc99c6c37a3c3015ee0aa7b41abe3bbc3c3ec2
StevenHuang2020 <61686583+StevenHuang2020@users.noreply.github.com>
2020-03-30 15:36:27 +1200

    Add files via upload

---
+++ common.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+def splitData(X,y):
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    print('x_train.shape = ', x_train.shape)
+    print('y_train.shape = ', y_train.shape)
+    print('x_test.shape = ', x_test.shape)
+    print('y_test.shape = ', y_test.shape)
+    return x_train, x_test, y_train, y_test
+
+def getCsvDataset(file,skipLines=3):
+    df = pd.read_csv(file, header=None,skiprows=skipLines)
+    #print(df.describe().transpose())
+    #print(df.head())
+
+    X, y = df.iloc[:, :-1].values, df.iloc[:, -1].values
+    return splitData(X,y)
+
+def printModelWeights(model):
+    print("*" * 50,'model paramters')
+    print('layers number=', len(model.layers))
+    for i in range(len(model.layers)):
+        print('layer',i,'-------------')
+        weights = model.layers[i].get_weights()
+        print('weight:', weights[0], 'b:',weights[1])
+        #for j in range(len(weights)):
+        #   print('weight',j,':',weights[j], end='')
+        #print('')
+        
+def main():
+    file = r'./db/fucDatasetReg_1F.csv'
+    getCsvDataset(file)
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ plotCommon.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+def plotLoss(loss,name='Loss'):
+    plt.title(name)
+    plt.plot(loss)
+    plt.show()
+
+def plotLosses(loss1,loss2,name='Losses'):
+    plt.title(name)
+    plt.plot(loss1)
+    plt.plot(loss2)
+    plt.show()
+
+def plotLossAndAcc(loss,acc,name='Loss&Acc'):
+    plt.title(name)
+    plt.plot(loss, label='loss')
+    plt.plot(acc, label='accuracy')
+    plt.legend()
+    plt.show()
+
+def plotSubLossAndAcc(loss,acc,name='Loss&Acc'):
+    plt.title(name)
+    ax = plt.subplot(1,2,1)
+    ax.plot(loss, label='loss')
+    ax.legend()
+    
+    ax = plt.subplot(1,2,2)
+    ax.plot(acc, label='accuracy',color='g')
+    ax.legend()
+
+    plt.show()
+    
+def plotResultError(scores_train,scores_test,loss):
+    error_train = np.array(1-np.array(scores_train))
+    #print(error_train)
+    error_test = np.array(1 - np.array(scores_test))
+
+    plt.title('Error&Loss over epochs use MLP')
+    plt.plot(error_train, label='error_train')
+    #plt.plot(error_test, label='error_test')
+    plt.plot(loss, label='loss')
+    plt.legend()
+    plt.show()
+
+def plotResult(scores_train,scores_test,loss):
+    if 0: #style 1
+        fig, ax = plt.subplots(3, sharex=True, sharey=True)
+        ax[0].plot(scores_train)
+        ax[0].set_title('Train mean accuracy')
+        ax[1].plot(scores_test)
+        ax[1].set_title('Test mean accuracy')
+        ax[2].plot(loss)
+        ax[2].set_title('Loss')
+        fig.suptitle("Accuracy &Loss over epochs", fontsize=14)
+    else:# style 2
+        plt.title('Acc&Loss over epochs use MLP')
+        plt.plot(scores_train, label='scores_train')
+        plt.plot(scores_test, label='scores_test')
+        plt.plot(loss, label='loss')
+        plt.legend()
+    plt.show()
+
+def plotTrainRes(x_train, y_train, x_test, y_test, loss):
+    ax = plt.subplot(1, 2, 1)
+    ax.title.set_text('training dataset')
+    plt.scatter(x_train[:,0], y_train, label='input dataset')  # plot train dataset
+
+    plt.plot(x_test[:,0],y_test,color='r')
+
+    ax = plt.subplot(1, 2, 2)
+    ax.title.set_text('Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.plot(loss)
+    plt.show()
+
+def test6Plot():
+    # createdata
+    N = 100
+    x_data = np.random.rand(N)
+    y_data = np.random.rand(N)
+    #plt.show(scatterplot(x_data, y_data,yscale_log=False))
+
+    #plt.show(lineplot(x_data, y_data, x_label = "x_label", y_label="y_label", title="title"))
+    plt.show(histogram(x_data, n_bins=10))
+    pass
+
+
+if __name__ == "__main__":
+    test6Plot()
